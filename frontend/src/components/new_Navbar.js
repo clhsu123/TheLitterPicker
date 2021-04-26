a3f539cbbdf9447dfe15e833c6d2392f24c10435
Che-Lun, Hsu <chelunh1@uci.edu>
2021-04-26 15:46:27 -0700

    Redesign navigation bar

--- frontend/src/components/new_Navbar.js
+++ frontend/src/components/new_Navbar.js
@@ -6,6 +6,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import SearchBar from './searchBar';
 
 const styles = {
     text: {
@@ -18,26 +19,31 @@ export class new_Navbar extends Component {
         const { classes } = this.props; 
         return (
             <AppBar>
-            <Toolbar>
-            <Grid container spacing = {1} className={classes.text}>
-                <Grid item xs>
-                <Button color="inherit" component={Link} to ="/login">
-                    Login
-                </Button>
-                </Grid>
-                <Grid item xs = {8}>
-                <Button color="inherit" component={Link} to ="/">
-                    Home
-                </Button>
-                </Grid>
-                <Grid item xs>
-                <Button color="inherit" component={Link} to ="/signup">
-                    Signup
-                </Button>
-                </Grid>
-            </Grid>
-            </Toolbar>
-        </AppBar>
+                <Toolbar>
+                    <Grid container spacing = {1} className={classes.text}>
+                        <Grid item xs>
+                            <Button color="inherit" component={Link} to ="/">
+                                TheLitterPicker.com
+                            </Button>
+                        </Grid>
+                        <Grid item xs = {8}>
+                            <SearchBar />
+                        </Grid>
+                        <Grid container item xs>
+                            <Grid item sm>
+                                <Button color="inherit" component={Link} to ="/signup">
+                                    Signup
+                                </Button>
+                            </Grid>
+                            <Grid item sm>
+                                <Button color="inherit" component={Link} to ="/login">
+                                    Login
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
         )
     }
 }
