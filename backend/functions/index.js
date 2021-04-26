b0bddb43c796ef31ceed3e88d558a73845512762
Che-Lun, Hsu <chelunh1@uci.edu>
2021-04-25 23:30:15 -0700

    Comment on backend api function

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,18 +1,25 @@
+//Import firebase build-in functions
 const functions = require("firebase-functions");
 const app = require('express')();
+
+//Import self-defined functions from other folders
 const FBAuth = require('./util/fbAuth');
 const { getAllScreams , postOneScream} = require('./handlers/scream');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/user');
 
 
 //Scream route
-app.get('/screams', getAllScreams);
-app.post('/scream', FBAuth, postOneScream);
+app.get('/screams', getAllScreams); //Fetch all datas (In JSON format) from collection "Scream"
+app.post('/scream', FBAuth, postOneScream); //Post one document onto collection "scream"
 
-// User route
-app.post('/signup', signup);
-app.post('/login', login);
+//User route
+app.post('/signup', signup); //Sign up function, once signed up, add a document to collection "users" and return a token (used to identify user)
+app.post('/login', login); //Login funtion, once logged in, return a token (used to identify user)
 app.post('/user/image', FBAuth, uploadImage)
 app.post('/user', FBAuth, addUserDetails)
 app.get('/user', FBAuth, getAuthenticatedUser);
+
+//Other route could be added below, you could refer to functions above to design new functions
+
+//This line exports API functions of firebse in HTTP form
 exports.api = functions.https.onRequest(app);
--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import AppIcon1 from '../images/phone.png';
+import AppIcon2 from '../images/mail.png';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
@@ -11,9 +13,11 @@ const styles = {
     pic: {
         textAlign: 'center'
     },
-    something: {
-        textAlign: 'center',
-        color: 'red'
+    icon: {
+        margin: 'auto'
+    },
+    prof_button: {
+        width: '10'
     }
 };
 
@@ -22,19 +26,83 @@ export class profile extends Component {
         const { classes } = this.props; 
         return (
             <Grid container spacing = {1}>
-                    <Grid item xs = {4}>
+                <Grid container item xs = {12}>
+                    <Grid item sm>
                         <p>Pic one</p>
                     </Grid>
-                    <Grid item xs = {4}>
+                    <Grid item sm>
                         <p>Pic two</p>
                     </Grid>
-                    <Grid item xs = {4}>
+                    <Grid item sm>
                         <p>Pic three</p>
                     </Grid>
-
-                    <Grid item xs = {12} className = {classes.something}>
-                        <h1>Something</h1>
+                </Grid>
+                <Grid container item xs = {12}>
+                    <Grid item xs = {3}> 
+                        <p>pic</p>
                     </Grid>
+                    <Grid item xs = {3}>
+                        <p>name</p>
+                    </Grid>
+                    <Grid item xs = {0.5} className = {classes.icon}> 
+                        <img src={AppIcon1} alt="phone" width="30" height="30" />
+                    </Grid>
+                    <Grid item xs = {3}>
+                        <p>Phone number</p>
+                    </Grid>
+                    <Grid item xs = {0.5} className = {classes.icon}>
+                        <img src={AppIcon2} alt="mail" width='30' height="30" />
+                    </Grid>
+                    <Grid item xs={2.5}>
+                        <p>chelunh1@uci.edu</p>
+                    </Grid>
+                </Grid>
+                <Grid container item xs = {12}>
+                    <Grid container item xs = {8}>
+                        <Grid container item xs = {8}>
+                        <Grid item sm>
+                            <Button variant="outlined" color="primary">
+                                Boys/Sires
+                            </Button>
+                        </Grid>
+                        <Grid item sm>
+                            <Button variant="outlined" color="primary">
+                                Girls/Dams
+                            </Button>
+                        </Grid>
+                        <Grid item sm>
+                            <Button variant="outlined" color="primary">
+                                Available Puppies
+                            </Button>
+                        </Grid>
+                        </Grid>
+                        <Grid container item xs = {8}>
+                            <h1>This is a website designed and developed by Eric Hsu, a currently enrolled graduate student at UC Irvine.  Go check my fucking codes.</h1>
+                        </Grid>
+                        <Grid container item xs = {8}>
+                            <p>Just some information regarding web development...</p>
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs = {4}>
+                        <Grid item xs className = {classes.prof_button}>
+                            <Button variant="contained" color="primary" fullWidth>
+                                Upadate Profile
+                            </Button>
+                            <br /><br />
+                            <Button variant="contained" color="primary" fullWidth>
+                                View Applications
+                            </Button>
+                            <br /><br />
+                            <Button variant="contained" color="primary" fullWidth>
+                                Customize Application Form
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                
+                <Grid container item xs = {12}>
+                    <h1>New features to be developed...</h1>
+                </Grid>
             </Grid>
         )
     }
